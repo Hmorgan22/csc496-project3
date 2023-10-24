ed7729f79fc13513af67e7eb597caef72db65119
Hmorgan22 <huntermorgan1219@yahoo.com>
2023-10-24 16:47:08 -0400

    updated css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import * as React from "react"
 import Navbar from "../components/Navbar"
 import { StaticImage } from "gatsby-plugin-image"
-
+import '../styles/custom.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const IndexPage = () => {
   return (
-    <Container>
+    <main>
       <Navbar />
-      <h1>Kourtney Morgan Photography</h1>
-      <h2>Hello & Welcome</h2>
-      <p>I believe choosing a photographer is a personal experience, so thank you for considering me. 
-      I promise that I will help you feel comfortable and confident in front of my camera, and we will create photographs that you’ll want to look back on again and again.​​​​​​​ 
-      Whether you are celebrating a milestone or want to capture a moment in time just because, please reach out, and let's have a conversation.</p>
-    </Container> 
+      <Container>
+        <h1>Kourtney Morgan Photography</h1>
+        <h2>Hello & Welcome</h2>
+        <p>I believe choosing a photographer is a personal experience, so thank you for considering me.
+          I promise that I will help you feel comfortable and confident in front of my camera, and we will create photographs that you’ll want to look back on again and again.
+          Whether you are celebrating a milestone or want to capture a moment in time just because, please reach out, and let's have a conversation.</p>
+      </Container>
+    </main>
+
   )
 }
 
